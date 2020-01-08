432aefaf76ac38813aae6ff3cabb637d47ed3d0f
Sachin Singh <45479287+sachinsiota@users.noreply.github.com>
2020-01-08 18:38:06 +0530

    auto complete added for recepients.

--- src/app/compose-dialog/compose-dialog.component.ts
+++ src/app/compose-dialog/compose-dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { DataServiceService } from '../data-service.service';
+
 
 @Component({
   selector: 'app-compose-dialog',
@@ -8,22 +13,55 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./compose-dialog.component.css']
 })
 export class ComposeDialogComponent implements OnInit {
-
+  allRecepients: string[] = ['Unit C202', 'Unit C203', 'Unit C204', 'Unit C205', 'Unit C206'];
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
   constructor(
+    public dataServiceService: DataServiceService,
     public dialogRef: MatDialogRef<ComposeDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { 
+  ) {
     dialogRef.disableClose = true;
   }
 
-  loginForm: FormGroup;
+  sentForm: FormGroup;
+    filteredRecepients: Observable<any[]>;
+    recepientsArray:[];
 
   ngOnInit() {
-    this.loginForm = new FormGroup({
+    this.sentForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
+      recipients: new FormControl,
     });
+    this.filteredRecepients = this.sentForm.get('recipients').valueChanges.pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : (value ? value.email : null)),
+        map(name => name ? this._filter(name) : this.dataServiceService.users)
+      )
   }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    console.log(event);
+    //this.recepientsArray.push(event.option.value);
+    // this.calculateUnitDetails();
+    // this.colonyUnits.splice(this.colonyUnits.indexOf(event.option.value), 1);
+    // console.log(this.units);
+    // console.log(this.colonyUnits);
+    // this.unitInput.nativeElement.value = '';
+    this.sentForm.get('recipients').setValue(null);
+  }
+
+  private _filter(value) {
+    console.log(value);
+    if(value){
+    const filterValue = value.toLowerCase();
+    return this.dataServiceService.users.filter(unit => unit.email.toLowerCase().indexOf(filterValue) === 0);
+  }
+}
 
   onFormSubmit() {
     if (this.loginForm.valid) {
@@ -37,4 +75,3 @@ export class ComposeDialogComponent implements OnInit {
   }
 
 }
-
